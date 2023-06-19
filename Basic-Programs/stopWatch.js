b81edcfa704bf63d9869a8addee5df4e3ad097aa
kirtan <kirtan1902@gmail.com>
2023-06-19 19:16:29 +0530

    updated stopwatch

--- Basic-Programs/stopWatch.js
+++ Basic-Programs/stopWatch.js
@@ -3,7 +3,10 @@
 
 
 //line 2-9 is an asynchronus code
+//timer using setInterval
 var count = 1;
+setTimeout(() => {  
+}, 1000);
 function timer() {
     console.clear();
     console.log(count);
@@ -11,7 +14,18 @@ function timer() {
 }
 setInterval(timer,1000);
 
-//line 15-19 is an synchronus code 
+
+//timer using setTimeout
+function counter(delay, count) {
+  console.log(count);
+  setTimeout(function () {
+    // console.clear();
+    counter(delay, ++count);
+  }, delay);
+}
+counter(1000, 1);
+
+// //line 15-19 is an synchronus code 
 var count2 = 1;
 for (let index = 0; index < 1000000000; index++) {
   count2+=1;
